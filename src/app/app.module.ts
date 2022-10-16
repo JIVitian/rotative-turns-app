e0ac350e84882065ce6ad826d534a9f0494affdf
JIVitian <jivitian@gmail.com>
2022-10-16 18:55:42 -0300

    Created employee module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    EmployeeModule,
     NavbarComponent,
     NgbModule,
-    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
