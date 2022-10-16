e5f42b0759ae02f7a25cbeb9e1cfbb465718edca
JIVitian <jivitian@gmail.com>
2022-10-16 20:18:10 -0300

    Workday service

--- src/app/modules/workday/workday.service.ts
+++ src/app/modules/workday/workday.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Workday } from './models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WorkdayService {
+  private readonly endpoint = environment.baseUrl + '/workday';
 
-  constructor() { }
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getAll() {
+    return this.httpClient.get(this.endpoint + '/all');
+  }
+
+  getById(id: number) {
+    return this.httpClient.get(this.endpoint + `/${id}`);
+  }
+
+  create(workday: Workday) {
+    return this.httpClient.post(this.endpoint, workday);
+  }
+
+  update(workday: Workday) {
+    return this.httpClient.patch(this.endpoint + '/update', workday);
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete(this.endpoint + `/delete/${id}`);
+  }
 }
