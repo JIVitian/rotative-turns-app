c24010251468ca28b445e48c59d19d2ca368ebae
JIVitian <jivitian@gmail.com>
2022-10-16 19:52:18 -0300

    Workday model

--- src/app/modules/workday/models/workday.ts
+++ src/app/modules/workday/models/workday.ts
@@ -1,1 +1,11 @@
-export type Workday = {};
+import { Employee } from '../../employee';
+import { WorkdayType } from '../../workday-type';
+
+export type Workday = {
+  id?: number;
+  employee: Employee;
+  type: WorkdayType;
+  date: Date;
+  timeFrom: Date;
+  timeTo: Date;
+};
