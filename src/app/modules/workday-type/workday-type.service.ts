6c1e9216a2b464fe6b767a8fd8db9d6ba20f47ac
JIVitian <jivitian@gmail.com>
2022-10-16 19:52:10 -0300

    Workday Type service

--- src/app/modules/workday-type/workday-type.service.ts
+++ src/app/modules/workday-type/workday-type.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+import { WorkdayType } from '../workday-type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WorkdayTypeService {
+  endpoint: string = environment.baseUrl + '/workday/type';
 
-  constructor() { }
+  constructor(private readonly httpClient: HttpClient) {}
+
+  getAll() {
+    return this.httpClient.get(this.endpoint + '/all');
+  }
+
+  getById(id: number) {
+    return this.httpClient.get(this.endpoint + `/${id}`);
+  }
+
+  create(workdayType: WorkdayType) {
+    return this.httpClient.post(this.endpoint, workdayType);
+  }
+
+  update(workdayType: WorkdayType) {
+    return this.httpClient.patch(this.endpoint + '/update', workdayType);
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete(this.endpoint + `/delete/${id}`);
+  }
 }
